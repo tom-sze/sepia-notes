9615e971719521631582417f94d03963c2c928f9
tom-sze <96094285+tom-sze@users.noreply.github.com>
2024-09-26 21:53:14 +0200

    lots of changes, almost done webapp

--- script.js
+++ script.js
@@ -4,24 +4,3 @@ function scrollSmoothTo(elementId) {
         behavior: 'smooth'
     });
 }
-
-function setTheme(themeName) {
-    localStorage.setItem('theme', themeName);
-    document.documentElement.className = themeName;
-}
-function toggleTheme() {
-    if (localStorage.getItem('theme') === 'dark-theme') {
-        setTheme('light-theme');
-    } else {
-        setTheme('dark-theme');
-    }
-}
-
-// set theme on initial load
-(function () {
-    if (localStorage.getItem('theme') === 'dark-theme') {
-        setTheme('dark-theme');
-    } else {
-        setTheme('light-theme');
-    }
-})();
